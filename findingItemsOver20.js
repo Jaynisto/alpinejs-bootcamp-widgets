7952a9f6a97c15d21aeb25abf9a2467a74c269e4
Fanie <faniejohnsons98@gmail.com>
2023-05-19 16:20:15 +0200

    Filtering

--- findingItemsOver20.js
+++ findingItemsOver20.js
@@ -2,7 +2,7 @@ function findItemsOver20(items){
     var over20 = [];
     for(var i = 0; i < items.length; i++){
       var listOfItems = items[i];
-      if(listOfItems.qty > 20){
+      if(listOfItems.quantity > 20){
         over20.push(listOfItems);
       }
     }
@@ -12,7 +12,7 @@ function findItemsOver20(items){
     var over = [];
     for(var i = 0; i < items.length; i++){
       var listOfItems = items[i];
-      if(listOfItems.qty > threshold){
+      if(listOfItems.quantity > threshold){
         over.push(listOfItems);
       }
     }
